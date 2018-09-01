be9293c574e67200479620a9bcffca6cc68d70a7
Dale Sedivec <dale@codefu.org>
2018-09-01 00:28:42 -0500

    Enable load-prefer-newer

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
+;;; Prologue
+
+;; Prefer loading a newer .el to an older .elc.  Probably keeps me
+;; from getting in trouble if I forget to byte compile.
+(setq load-prefer-newer t)
+
+
+;;; package.el
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
