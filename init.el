84f2418b2790ca54a4ef292420de6578fd8c3bb1
Dale Sedivec <dale@codefu.org>
2018-09-01 00:29:10 -0500

    Get customize stuff out of my init.el

--- init.el
+++ init.el
@@ -4,6 +4,11 @@
 ;; from getting in trouble if I forget to byte compile.
 (setq load-prefer-newer t)
 
+;; Set this early before I potentially install packages, which will
+;; modify customizable variable `package-selected-packages'.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
 
 ;;; package.el
 (require 'package)
