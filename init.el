fbac2b79e31d68c34eab9b7a70576de54f02a08d
Dale Sedivec <dale@codefu.org>
2018-09-02 03:18:16 -0500

    Make customizations.el compatible with my Spacemacs

--- init.el
+++ init.el
@@ -31,11 +31,15 @@
 
 ;; Set this early before I potentially install packages, which will
 ;; modify customizable variable `package-selected-packages'.
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(setq custom-file (expand-file-name "customizations.el"
+                                    (my:if-spacemacs
+                                        dotspacemacs-directory
+                                      user-emacs-directory)))
 (load custom-file)
 
 
 ;;; package.el
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
