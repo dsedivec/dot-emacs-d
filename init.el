75848de799f8210c02d57f0b9f95a17559b6e490
Dale Sedivec <dale@codefu.org>
2018-10-11 20:40:20 -0500

    Fancy Unicode display of window number from winum
    Idea stolen from Spaceline.

--- init.el
+++ init.el
@@ -1013,7 +1013,32 @@ for this command) must be an arrow key."
 
 ;;; winum
 
-(winum-mode)
+(setq winum-auto-setup-mode-line nil)
+
+(winum-mode 1)
+
+(defvar my:winum-number-string-base #x2780)
+
+(defvar my:winum-number-string-min 1)
+
+(defvar my:winum-number-string-max 10)
+
+(defun my:winum-get-number-string (&optional window)
+  (let* ((n (winum-get-number window))
+         (s (if (numberp n)
+                (concat
+                 (if (and (>= n my:winum-number-string-min)
+                          (<= n my:winum-number-string-max))
+                     (char-to-string (+ my:winum-number-string-base
+                                        (- n my:winum-number-string-min)))
+                   (int-to-string n))
+                 " ")
+              "")))
+    (propertize s 'face 'winum-face)))
+
+(push '(:eval (my:winum-get-number-string)) (cdr mode-line-format))
+
+(set-face-attribute 'winum-face nil :height 1.2)
 
 (with-eval-after-load 'winum
   (bind-keys :map winum-keymap
