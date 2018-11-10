6cf9b6e155a40375d0942f355f19a0b506ae7841
Dale Sedivec <dale@codefu.org>
2018-11-09 18:40:25 -0600

    More multiple-cursors commands

--- .mc-lists.el
+++ .mc-lists.el
@@ -12,10 +12,12 @@
         mwim-beginning-of-code-or-line
         mwim-end-of-line-or-code
         my:insert-tab-or-spaces
+        my:python-magic-single-quote
         org-beginning-of-line
         org-self-insert-command
         paredit-backward-kill-word
         paredit-forward
+        paredit-forward-delete
         paredit-forward-kill-word
         paredit-raise-sexp
         paredit-semicolon
