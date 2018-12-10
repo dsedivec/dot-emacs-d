7dec0a4f5872eb856cd58dd5087c41e1c8a0c129
Dale Sedivec <dale@codefu.org>
2018-12-09 18:15:16 -0600

    Factor out my bookmark+ speed-ups into a recipe

--- init.el
+++ init.el
@@ -766,35 +766,7 @@ it returns the node that your EDIT-FORM changed)."
   (add-to-list 'which-key-replacement-alist
                '(("C-x p t" . "Prefix Command") . (nil . "tags"))))
 
-(defvar my:bookmark--in-bookmark-save nil
-  "Set to true only within a call to `bookmark-save'.")
-
-;; Maybe I should submit this upstream.
-(define-advice customize-save-variable
-    (:before-until (variable value &rest args) my:speed-up-bookmark-save)
-  "Within `bookmark-save', only save when VARIABLE != VALUE.
-This speeds up bookmark+ which tries to save this variable within
-`bookmark-save' when `bmkp-last-as-first-bookmark-file' is
-non-nil."
-  (and my:bookmark--in-bookmark-save
-       (boundp variable)
-       (equal (symbol-value variable) value)))
-
-(define-advice bookmark-save
-    (:around (orig-fun &rest args) my:speed-up-bookmark-save)
-  "Hacks to speed up `bookmark-save'.
-This is all very brittle, but makes saving bookmarks maybe ~14x
-faster, in my testing."
-  ;; Neuter `vc-mode' and `magit-auto-revert-mode' `find-file-hook's
-  ;; for a file we are (presumably) only opening transiently, that
-  ;; being our bookmarks file.  They are slow, relatively speaking.
-  (cl-letf ((vc-handled-backends nil)
-            ;; This is really a fairly offensive way to turn
-            ;; `magit-auto-revert-mode-enable-in-buffers' into a noop.
-            ;; I should do better.
-            ((default-value 'auto-revert-mode-set-explicitly) t)
-            (my:bookmark--in-bookmark-save t))
-    (apply orig-fun args)))
+(my:load-recipes 'bookmark+-speed-up)
 
 
 ;;; bs
