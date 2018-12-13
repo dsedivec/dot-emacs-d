3b0c106e512060c3769052d070eb6666e2caa385
Dale Sedivec <dale@codefu.org>
2018-12-13 15:23:13 -0600

    Fix sorting package-selected-packages in Emacs ~27
    Looks like customize will now write `'(value)' instead of
    `(quote (value))` in Emacs master.  Without these changes it'll
    completely hork up your custom settings file.  Now it should work both
    in Emacs 26 and in master.

--- recipes/custom-format-selected-packages.el
+++ recipes/custom-format-selected-packages.el
@@ -14,7 +14,8 @@
     (let ((err-fmt "In `my:selected-packages-one-per-line' advice: %S")
           (this-buf (current-buffer))
           form
-          pos)
+          pos
+          packages)
       ;; `with-demoted-errors' deployed *strategically* throughout
       ;; this advice to avoid blowing up `custom-save-variables'
       ;; because we didn't find exactly what we were looking for.  I
@@ -22,55 +23,71 @@
       ;; because once we start modifying the buffer, you probably need
       ;; to know about errors since we might have just made the
       ;; `custom-set-variables' form invalid.
-      (with-demoted-errors err-fmt
-        ;; `custom-save-variables' should have left us right at the
-        ;; end of the `custom-set-variables' form that it inserted.
-        (forward-sexp -1)
-        (unless (looking-at-p "(custom-set-variables\\_>")
-          (error "Expected to be at `custom-set-variables' form"))
-        ;; Move into the list of variables.
-        (down-list)
-        (forward-sexp)
-        ;; Look for `package-selected-packages' in the list of
-        ;; variables.
-        (condition-case err
-            ;; Yeah, this is ugly.  Read carefully and note that we
-            ;; save the whole `package-selected-packages' form in
-            ;; `form'.
-            (while (not (eq (caadr (setq form (read this-buf)))
-                            'package-selected-packages)))
-          (t (error "Couldn't find `package-selected-packages': %S" err))))
-      (pcase form
-        ;; Make sure this form looks like how we expect:
+      (when
+          (with-demoted-errors err-fmt
+            ;; `custom-save-variables' should have left us right at
+            ;; the end of the `custom-set-variables' form that it
+            ;; inserted.
+            (forward-sexp -1)
+            (unless (looking-at-p "(custom-set-variables\\_>")
+              (error "Expected to be at `custom-set-variables' form"))
+            ;; Move into the list of variables.
+            (down-list)
+            (forward-sexp)
+            ;; Look for `package-selected-packages' in the list of
+            ;; variables.
+            (condition-case err
+                ;; Yeah, this is ugly.  Read carefully and note that
+                ;; we save the whole `package-selected-packages' form
+                ;; in `form'.
+                (while (not (eq (caadr (setq form (read this-buf)))
+                                'package-selected-packages)))
+              (t (error "Couldn't find `package-selected-packages': %S" err)))
+            (pcase form
+              (`(quote (package-selected-packages
+                        (quote ,(and (pred listp) pcase-packages))))
+               (setq packages pcase-packages))
+              (_
+               (error "`package-selected-packages' has unexpected value: %S"
+                      form)))
+            ;; Move back to beginning of '(package-selected packages
+            ;; ...) because `read' moved us to the end of it
+            (forward-sexp -1)
+            ;; Move inside the `package-selected-packages' entry.
+            (down-list 1)
+            ;; Emacs 26 writes it as "(quote (list of packages))".
+            ;; Emacs 27 writes it as "'(list of packages)".  Perform
+            ;; accordingly.  ">" is "end of comment" and is needed
+            ;; here to skip over newlines.
+            (skip-syntax-forward "w_->")
+            (cond
+              ((looking-at-p "(quote")
+               ;; Enter this extra list that doesn't exist in Emacs 27.
+               (down-list 1))
+              ((not (eq (char-after) ?'))
+               (error (concat "Can't find list of packages in"
+                              " `package-selected-packages' stuck at pos %d")
+                      (point))))
+            ;; Enter the list of packages
+            (down-list 1)
+            ;; Now go back up to its opening parenthesis
+            (backward-up-list 1)
+            ;; Success, ready to rewrite the list of packages
+            t)
+        ;; Not going to use `with-demoted-errors' after this since
+        ;; we're going to start modifying the buffer.
         ;;
-        ;;     '(package-selected-packages '(<list of packages>))
-        (`(quote (package-selected-packages
-                  (quote ,(and (pred listp) packages))))
-         (with-demoted-errors err-fmt
-           ;; Move back to beginning of '(package-selected packages
-           ;; ...) because `read' moved us to the end of it
-           (forward-sexp -1)
-           ;; Go inside the list of packages
-           (down-list 3)
-           ;; Go to the opening parenthesis of the list of packages
-           (backward-up-list 1))
-         ;; Not going to use `with-demoted-errors' after this since
-         ;; we're going to start modifying the buffer.
-         ;;
-         ;; Insert the package list with one package per line
-         (insert "(")
-         (newline-and-indent)
-         (dolist (package (cl-sort packages #'string< :key #'symbol-name))
-           (prin1 package this-buf)
-           (newline-and-indent))
-         (insert ")")
-         ;; Delete the old list of packages (we inserted in front of it)
-         (setq pos (point))
-         (forward-sexp)
-         (delete-region pos (point)))
-        (_
-         (error "`package-selected-packages' doesn't look as expected: %S"
-                form))))))
+        ;; Insert the package list with one package per line
+        (insert "(")
+        (newline-and-indent)
+        (dolist (package (cl-sort packages #'string< :key #'symbol-name))
+          (prin1 package this-buf)
+          (newline-and-indent))
+        (insert ")")
+        ;; Delete the old list of packages (we inserted in front of it)
+        (setq pos (point))
+        (forward-sexp)
+        (delete-region pos (point))))))
 
 (advice-add 'custom-save-variables :after
             #'my:custom-save-selected-packages-sorted-one-per-line)
