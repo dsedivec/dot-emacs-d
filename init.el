315f98f53770cc86c4282e2f64a9544d1c572600
Dale Sedivec <dale@codefu.org>
2018-12-19 10:27:39 -0600

    Delete auto-save files when reverting the buffer
    Emacs was leaving `#foo#` all over the FS if I reverted a modified
    buffer after it had been auto-saved.  Bad Emacs.

--- init.el
+++ init.el
@@ -1064,6 +1064,19 @@ surround \"foo\" with (in this example) parentheses.  I want
 
 (setq make-backup-files nil)
 
+;; Reverting a buffer doesn't delete its auto-save file?  Well, OK,
+;; fine, I'll delete it.
+(defun my:delete-auto-save-after-revert ()
+  (let ((auto-save-file-name (make-auto-save-file-name)))
+    (when (and (file-exists-p auto-save-file-name)
+               ;; Buffer really shouldn't be modified, just being
+               ;; extra-safe here.
+               (not (buffer-modified-p)))
+      (message "Deleting auto save file %s after revert" auto-save-file-name)
+      (delete-file auto-save-file-name))))
+
+(add-hook 'after-revert-hook #'my:delete-auto-save-after-revert)
+
 
 ;;; find-func
 
