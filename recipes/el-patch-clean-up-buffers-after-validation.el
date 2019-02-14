e1757c8a29a00c177e8758be7910fbad7f0ae9cd
Dale Sedivec <dale@codefu.org>
2019-02-14 09:02:38 -0600

    Stop el-patch buffer clean-up killing random buffers
    See comment added for details.

--- recipes/el-patch-clean-up-buffers-after-validation.el
+++ recipes/el-patch-clean-up-buffers-after-validation.el
@@ -1,39 +1,62 @@
 ;; -*- lexical-binding: t; -*-
 
 ;; `el-patch-validate' can/will open buffers and leave them open,
-;; which is irritating.  Kill buffers opened by `el-patch-validate'.
+;; which is irritating.
+;;
+;; I am amused that use of el-patch itself is probably the best way to
+;; fix this problem, though really I should just kick this upstream.
+;; Originally I put some advice around `el-patch-validate' that added
+;; a temporary `find-file-hook' that watched for files being opened.
+;; Unfortunately, at one point (startup, actually) I called
+;; `el-patch-validate' and somehow a timer/hook that opened some
+;; unrelated files fired in the middle of `el-patch-validate' (I
+;; think), and I ended up killing a bunch of buffers I cared about.
+;; That's how we landed on the below fix.
 
-(defvar my:el-patch-validate--buffers-opened)
+(require 'el-patch)
 
-(defvar my:el-patch-validate--depth 0)
+(el-patch-defun el-patch-locate-variable (definition)
+  "Return the source code of DEFINITION.
+DEFINITION is a list starting with `defvar' or similar."
+  (el-patch-let (($finder (find-variable-noselect (nth 1 definition))))
+    (el-patch-swap
+      (el-patch-wrap-locator
+        $finder)
+      (let ((my:existing-buffers (buffer-list))
+            my:el-patch-buf)
+        (prog1
+            (el-patch-wrap-locator
+              (let ((my:buffer-point $finder))
+                (setq my:el-patch-buf (car my:buffer-point))
+                my:buffer-point))
+          ;; Note that `el-patch-wrap-locator' does a "horrifying
+          ;; bit of hackery" to try and make sure it gets a fresh
+          ;; buffer, not an existing one, but it doesn't work.  I
+          ;; think `find-file-visit-truename' might be to blame.
+          ;; Anyway, we have to make sure not to kill an existing
+          ;; buffer.  This is ugly.
+          (when (and my:el-patch-buf
+                     (not (memq my:el-patch-buf my:existing-buffers)))
+            (kill-buffer my:el-patch-buf)))))))
 
-(defun my:el-patch-validate--record-new-buffer ()
-  (push (current-buffer) my:el-patch-validate--buffers-opened))
+(el-patch-defun el-patch-locate-function (definition)
+  "Return the source code of DEFINITION.
+DEFINITION is a list starting with `defun' or similar."
+  (el-patch-let (($finder
+                  (find-function-noselect (nth 1 definition) 'lisp-only)))
+    (el-patch-swap
+      (el-patch-wrap-locator
+        $finder)
+      (let ((my:existing-buffers (buffer-list))
+            my:el-patch-buf)
+        (prog1
+            (el-patch-wrap-locator
+              (let ((my:buffer-point $finder))
+                (setq my:el-patch-buf (car my:buffer-point))
+                my:buffer-point))
+          (when (and my:el-patch-buf
+                     (not (memq my:el-patch-buf my:existing-buffers)))
+            (kill-buffer my:el-patch-buf)))))))
 
-(define-advice el-patch-validate
-    (:around (orig-fun &rest args) my:dont-leave-buffers-open)
-  ;; I don't actually expect to re-enter `el-patch-validate', but why
-  ;; risk it?
-  (let ((my:el-patch-validate--depth
-         (if (>= my:el-patch-validate--depth 0)
-             (1+ my:el-patch-validate--depth)
-           (warn "`el-patch-validate--depth has illegal value %S"
-                 my:el-patch-validate--depth)
-           0))
-        my:el-patch-validate--buffers-opened)
-    (unwind-protect
-         (progn
-           ;; AFAIK `find-file-hook' is only called when a new buffer
-           ;; is created, not when an existing buffer is used.  BTW
-           ;; the only reason I know of that `el-patch-validate' will
-           ;; ever open a *file* (not just a buffer) is to read the
-           ;; source.
-           (add-hook 'find-file-hook #'my:el-patch-validate--record-new-buffer)
-           (apply orig-fun args))
-      (when (<= my:el-patch-validate--depth 1)
-        (remove-hook 'find-file-hook
-                     #'my:el-patch-validate--record-new-buffer))
-      (dolist (buf my:el-patch-validate--buffers-opened)
-        (when (and (buffer-live-p buf)
-                   (not (buffer-modified-p buf)))
-          (kill-buffer buf))))))
+(el-patch-validate 'el-patch-locate-variable 'defun t)
+(el-patch-validate 'el-patch-locate-function 'defun t)
