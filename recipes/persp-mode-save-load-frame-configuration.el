451622743a28c8709b302e6b9fda3f05cc78680f
Dale Sedivec <dale@codefu.org>
2019-02-14 09:07:37 -0600

    Fix persp-mode frameset saving
    I didn't notice at exit that persp-mode was bitching about
    un-printable values in the `frameset-save` value I was putting in a
    property.  (How would I?  It logs it... then Emacs exits.)  Now we go
    out of our way not to put anything un-printable (really un-readable)
    in there.
    
    Another bug fixed: need to put the property on the default/nil
    perspective, just so we know where to find it to restore.  Otherwise
    it'll get stashed in whatever the current perspective is at exit, but
    then at startup it'll be in the nil perspective so you won't find
    it (unless you were in nil perspective when you exited---which I was
    in most of my testing, clearly).

--- recipes/persp-mode-save-load-frame-configuration.el
+++ recipes/persp-mode-save-load-frame-configuration.el
@@ -1,16 +1,58 @@
 ;; -*- lexical-binding: t; -*-
 
+(require 'cl-lib)
+(require 'subr-x)
+
 ;; Save/restore frame configuration along with persp-mode.  Adding
 ;; data to perspectives inspired by
 ;; https://gist.github.com/gilbertw1/8d963083efea41f28bfdc85ed3c93eb4.
+;; We mustn't leave any frame parameters in the frameset that
+;; persp-mode thinks will print unreadably, otherwise it will not save
+;; our frameset when it saves perspectives.
+
+(defun my:persp-mode-serializable-p (obj)
+  (and (or (memq (type-of obj) '(bool-vector
+                                 char-table
+                                 cons
+                                 float
+                                 integer
+                                 string
+                                 symbol
+                                 vector)))
+       ;; Can't pass an improper list to `seq-every-p', but
+       ;; `sequencep' returns T for improper lists.
+       (or (and (consp obj)
+                (my:persp-mode-serializable-p (car obj))
+                (my:persp-mode-serializable-p (cdr obj)))
+           (not (sequencep obj))
+           (seq-every-p #'my:persp-mode-serializable-p obj))))
+
+(defun my:persp-mode-unserializable-frame-parameters (&optional frame)
+  (mapcar #'car (seq-remove (lambda (pair)
+                              (my:persp-mode-serializable-p (cdr pair)))
+                            (frame-parameters frame))))
+
+(defun my:persp-mode-all-unserializable-frame-parameters ()
+  (seq-reduce #'cl-union
+              (mapcar #'my:persp-mode-unserializable-frame-parameters
+                      (frame-list))
+              nil))
+
+(defun my:persp-mode-get-frameset ()
+  (let ((frameset-filter-alist
+         (append (mapcar (lambda (prop) (cons prop :never))
+                         (my:persp-mode-all-unserializable-frame-parameters))
+                 frameset-filter-alist)))
+    (frameset-save nil)))
 
 (defun my:persp-mode-save-frame-configuration (&rest _)
-  (set-persp-parameter 'my:frame-configuration (frameset-save nil)))
+  (set-persp-parameter 'my:frame-configuration (my:persp-mode-get-frameset)
+                       nil))
 
 (defun my:persp-mode-restore-frame-configuration (&rest _)
   (condition-case e
-      (when-let ((frameset (persp-parameter 'my:frame-configuration)))
-        (delete-persp-parameter 'my:frame-configuration)
+      (when-let ((frameset (persp-parameter 'my:frame-configuration nil)))
+        (delete-persp-parameter 'my:frame-configuration nil)
         (frameset-restore frameset :reuse-frames t :cleanup-frames t))
     (t
      (warn "failed to restore frames: %S" e))))
