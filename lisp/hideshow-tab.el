fd43e0a714bc96ddbecbb5ef02b3619de8cf9554
Dale Sedivec <dale@codefu.org>
2019-04-26 13:31:27 -0500

    Add "Version" to hideshow-tab.el

--- lisp/hideshow-tab.el
+++ lisp/hideshow-tab.el
@@ -4,6 +4,7 @@
 
 ;; Author: Dale Sedivec <dale@codefu.org>
 ;; Keywords: convenience
+;; Version: 0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
