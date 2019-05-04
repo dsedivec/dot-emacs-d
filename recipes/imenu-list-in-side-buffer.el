ddc3fec6a2a884ce4adf730a2eb255dab97834b7
Dale Sedivec <dale@codefu.org>
2019-05-04 16:40:59 -0500

    Fix and improve imenu-list-in-side-buffer recipe
    * It was providing `imenu-list-position` for the `side` alist entry in
      `display-buffer-alist`.  `imenu-list-position` takes `above` and
      `below`, but `side` wants `top` or `bottom`, respectively.  This is
      now fixed.
    
    * Now respects `imenu-list-buffer-name`.
    
    * It was passing the `window-width` to `display-buffer` always, even
      when displaying on the top or bottom, which was incorrect.  It now
      passes either `window-width` or `window-height` as appropriate.
    
    * Now installs the `display-buffer-alist` entry if `imenu-list` has
      already been loaded at the time this recipe is loaded.
    
    * Doc string and comments also corrected/improved.

--- recipes/imenu-list-in-side-buffer.el
+++ recipes/imenu-list-in-side-buffer.el
@@ -4,30 +4,49 @@
 ;; `display-buffer' show `imenu-list' in a side window of
 ;; `imenu-list-size' width/height.
 ;;
-;; This also sets the new side window to be dedicated and marks that
-;; its height/width should be preserved, as appropriate for the value
-;; of `imenu-list-position'.
+;; This also sets the new side window to be a dedicated window and
+;; preserves the window's height/width (see `window-preserve-size'),
+;; as appropriate for the value of `imenu-list-position'.
 ;;
 ;; You will probably need to call
 ;; `my:imenu-list-install-display-buffer' if you change
 ;; `imenu-list-position' or `imenu-list-size'.
-;;
-;; I think this somewhat changes the meaning of `imenu-list-size',
-;; because *I think* before this advice it specified the text
-;; width/height, and after this advice it specifies the total
-;; width/height.  See `(elisp)Windows Sizes' for the difference.
+
+(require 'cl-lib)
 
 (defun my:imenu-list-install-display-buffer ()
-  "Put imenu-list on a dedicated left side window with a fixed size."
-  (setf (alist-get "^\\*Ilist\\*$" display-buffer-alist nil nil #'equal)
-        `(display-buffer-in-side-window
-          (side . ,imenu-list-position)
-          (window-width . ,imenu-list-size)
-          ;; Means preserve width, not height.  I think.
-          (preserve-size . ,(if (memq imenu-list-position '(left right))
-                                '(t . nil)
-                              '(nil . t)))
-          (dedicated . t))))
+  "Put imenu-list buffer on a dedicated side window with a preserved size."
+  (let* ((side (cl-ecase imenu-list-position
+                 (above 'top)
+                 (below 'bottom)
+                 ((left right) imenu-list-position)))
+         (preserve-dimen (if (memq side '(left right))
+                             'window-width
+                           'window-height)))
+    (setf (alist-get (concat "^" (regexp-quote imenu-list-buffer-name) "$")
+                     display-buffer-alist nil nil #'equal)
+          `(display-buffer-in-side-window
+            (side . ,side)
+            ;; It is not totally clear to me if `imenu-list-size' is
+            ;; supposed to be the window's body height/width or the
+            ;; window's total height/width.  The way we're using it
+            ;; here it is definitely the total, not the body.
+            ;;
+            ;; If it's supposed to be the body height/width then I
+            ;; think it might be best to pass a function as the value
+            ;; for the `window-height'/`window-width' here, and then
+            ;; have the function compute it once the imenu-list window
+            ;; has come into existence.  (See documentation for those
+            ;; alist entries in the docstring for `display-buffer'.)
+            (,preserve-dimen . ,imenu-list-size)
+            (preserve-size . ,(if (eq preserve-dimen 'window-width)
+                                  '(t . nil)
+                                '(nil . t)))
+            (dedicated . t)))))
 
 (advice-add 'imenu-list-install-display-buffer :override
             #'my:imenu-list-install-display-buffer)
+
+(when (featurep 'imenu-list)
+  ;; Too late, imenu-list already loaded.  Call our new setup function.
+  (my:imenu-list-install-display-buffer))
