fc2897e2ecb26383adba081c433f0de78b94a145
Dale Sedivec <dale@codefu.org>
2020-06-30 23:57:58 -0500

    Remove Paradox fix, done upstream now

--- init.el
+++ init.el
@@ -2748,14 +2748,6 @@ See URL `https://www.terraform.io/docs/commands/validate.html'."
 (setq package-build-recipes-dir "~/repositories/melpa/recipes")
 
 
-;;; paradox
-
-;; Temporary fix for https://github.com/Malabarba/paradox/issues/181
-(if (fboundp 'package-menu-filter)
-    (warn "Paradox has fixed `package-menu-filter', remove fix from init.el")
-  (defalias 'package-menu-filter 'package-menu-filter-by-keyword))
-
-
 ;;; paredit
 
 (with-eval-after-load 'paredit
