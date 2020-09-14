f8a951cc551277c44ea6bd642310f977c8844cb7
Dale Sedivec <dale@codefu.org>
2020-09-13 23:54:11 -0500

    Potentially activate dumb-jump in all buffers
    This includes, for example, dired buffers, which don't call
    find-file-hook.

--- recipes/dumb-jump-only-if-no-tags.el
+++ recipes/dumb-jump-only-if-no-tags.el
@@ -6,25 +6,16 @@
 
 (require 'cl-lib)
 
-(defun my:dumb-jump-activate-unless-TAGS-exists ()
-  (unless (and buffer-file-name
-               (locate-dominating-file buffer-file-name "TAGS"))
-    (if (local-variable-p 'xref-backend-functions)
-        (progn
-          (setq xref-backend-functions (copy-sequence xref-backend-functions))
-          (let* ((insert-ptr (cl-member-if (lambda (el)
-                                             (memq el '(t etags--xref-backend)))
-                                           xref-backend-functions)))
-            (if insert-ptr
-                ;; Insert it before the (probably) default etags
-                ;; backend.
-                (setf (cdr insert-ptr) (cons (car insert-ptr) (cdr insert-ptr))
-                      (car insert-ptr) #'dumb-jump-xref-activate)
-              ;; No insertion point found, just put dumb-jump at the
-              ;; end.
-              (setq xref-backend-functions
-                    (nconc xref-backend-functions
-                           '(dumb-jump-xref-activate))))))
-      (add-hook 'xref-backend-functions #'dumb-jump-xref-activate nil t))))
+(defvar my:use-dumb-jump-this-buffer 'unknown)
+(make-variable-buffer-local 'my:use-dumb-jump-this-buffer)
 
-(add-hook 'find-file-hook #'my:dumb-jump-activate-unless-TAGS-exists)
+(defun my:dumb-jump-activate-unless-TAGS-exists ()
+  (when (eq my:use-dumb-jump-this-buffer 'unknown)
+    (setq my:use-dumb-jump-this-buffer
+          (if (and buffer-file-name
+                   (locate-dominating-file buffer-file-name "TAGS"))
+              nil
+            (dumb-jump-xref-activate))))
+  my:use-dumb-jump-this-buffer)
+
+(add-hook 'xref-backend-functions #'my:dumb-jump-activate-unless-TAGS-exists)
