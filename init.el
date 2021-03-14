538388bbb19997c17f9e745685ac127a95bd8c09
Dale Sedivec <dale@codefu.org>
2021-03-13 19:50:22 -0600

    Move org-mode C-e fix into a recipe

--- init.el
+++ init.el
@@ -2936,7 +2936,8 @@ See URL `https://www.terraform.io/docs/commands/validate.html'."
    'org-insert-heading-ignore-invisibility
    'org-jump-over-priority-after-setting-it
    'org-make-nice-id-from-headline-text
-   'org-property-drawer-fixes)
+   'org-property-drawer-fixes
+   'org-fix-C-e)
 
   (bind-keys :map org-mode-map
              ("C-c a" . org-agenda)
@@ -3002,36 +3003,6 @@ See URL `https://www.terraform.io/docs/commands/validate.html'."
   (my:load-recipes 'org-compact-clock-in-mode-line
                    'org-clock-update-mode-line-on-clock-in-out
                    'org-switch-to-pending-on-clock-in))
-
-;; As of 45048eb78 I guess org-end-of-line started working, and now
-;; C-e is bringing me to the end of my headline with collapsed
-;; content, i.e. before the ellipsis.  This infurates me when I
-;; usually want to "C-e M-RET" to add a new sibling headline to the
-;; collapsed one.  (Maybe I should come up with a new key
-;; combination for that.)  Give me back the old behavior: move to
-;; after the ellipsis.
-;;
-;; Further hack required if you want the 'special behavior of
-;; org-special-ctrl-a/e to work right: if we are at end of line
-;; you'll never be toggled back over to the end of the headline text
-;; if you keep hitting C-e.  This is because org-end-of-line only
-;; toggles if you're at the end of the line and not the end of the
-;; *visible* line (which, for a collapsed headline, means you're
-;; actually some lines further on).  So as a *hack*, move back to
-;; the real EOL, before the ellipsis, before executing
-;; org-end-of-line.
-(define-advice org-end-of-line
-    (:around (orig-fun &rest args) my:end-of-line-move-past-ellipsis)
-  (when (and (eq this-command last-command)
-             (= (point) (save-excursion
-                          (end-of-visible-line)
-                          (point))))
-    (beginning-of-visual-line)
-    (end-of-line))
-  (prog1
-      (apply orig-fun args)
-    (when (and (eolp) (org-invisible-p))
-      (end-of-visual-line))))
 
 ;; You can use this as a :formatter in clocktables where you've
 ;; forcibly narrowed headlines and don't want bracketed links to be
---
+++ recipes/org-fix-C-e.el
@@ -0,0 +1,32 @@
+;; -*- lexical-binding: t; -*-
+
+;; As of 45048eb78 I guess org-end-of-line started working, and now
+;; C-e is bringing me to the end of my headline with collapsed
+;; content, i.e. before the ellipsis.  This infurates me when I
+;; usually want to "C-e M-RET" to add a new sibling headline to the
+;; collapsed one.  (Maybe I should come up with a new key
+;; combination for that.)  Give me back the old behavior: move to
+;; after the ellipsis.
+;;
+;; Further hack required if you want the 'special behavior of
+;; org-special-ctrl-a/e to work right: if we are at end of line
+;; you'll never be toggled back over to the end of the headline text
+;; if you keep hitting C-e.  This is because org-end-of-line only
+;; toggles if you're at the end of the line and not the end of the
+;; *visible* line (which, for a collapsed headline, means you're
+;; actually some lines further on).  So as a *hack*, move back to
+;; the real EOL, before the ellipsis, before executing
+;; org-end-of-line.
+
+(define-advice org-end-of-line
+    (:around (orig-fun &rest args) my:end-of-line-move-past-ellipsis)
+  (when (and (eq this-command last-command)
+             (= (point) (save-excursion
+                          (end-of-visible-line)
+                          (point))))
+    (beginning-of-visual-line)
+    (end-of-line))
+  (prog1
+      (apply orig-fun args)
+    (when (and (eolp) (org-invisible-p))
+      (end-of-visual-line))))
