8169b37de881cd8abc83110c4c970c1f80d59bde
Dale Sedivec <dale@codefu.org>
2021-04-01 01:19:01 -0500

    Apparently JSX is a little off in js2-mode
    See comment.

--- init.el
+++ init.el
@@ -2509,8 +2509,11 @@ See URL `https://www.terraform.io/docs/commands/validate.html'."
 
 ;; https://elpa.gnu.org/packages/js2-mode.html
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+;; https://github.com/mooz/js2-mode/issues/529
+;; (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+(add-hook 'js-mode-hook #'js2-minor-mode)
 
 (defun my:js2-mode-hook ()
   ;; Prevailing JS style seems to be indenting with spaces.
