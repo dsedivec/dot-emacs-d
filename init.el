1342f9a42d27a916081a7c62839dfc714dd713bb
Dale Sedivec <dale@codefu.org>
2024-08-26 01:31:34 -0500

    Enable ahs-beginning-of-defun mode in Fennel

--- init.el
+++ init.el
@@ -1072,7 +1072,8 @@ Makes it hard to use things like `mc/mark-more-like-this-extended'."
   ;; Must trigger defcustom's :set.
   (customize-set-variable 'ahs-idle-interval 0.5)
 
-  (add-to-list 'ahs-plugin-bod-modes 'python-mode)
+  (dolist (mode '(python-mode fennel-mode))
+    (add-to-list 'ahs-plugin-bod-modes mode))
 
   (add-to-list 'ahs-inhibit-face-list 'font-lock-keyword-face)
 
