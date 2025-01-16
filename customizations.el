bae08a26258512fda0d44b90017ceca8bfd942ea
Dale Sedivec <dale@codefu.org>
2025-01-16 11:08:54 -0600

    Random changes to customization file
    Allow both `sql-dialect` and `sql-product`, I guess.  `sql-product` is
    supposed to be safe already!

--- customizations.el
+++ customizations.el
@@ -5,12 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(ahs-idle-interval 0.5)
  '(org-modules
-   '(org-tempo org-id ol-w3m ol-bbdb ol-bibtex ol-docview ol-gnus ol-info ol-irc ol-mhe ol-rmail ol-eww))
+   '(org-tempo org-id ol-w3m ol-bbdb ol-bibtex ol-docview ol-gnus ol-info
+	       ol-irc ol-mhe ol-rmail ol-eww))
  '(package-selected-packages nil)
  '(safe-local-variable-values
-   '((sql-dialect . ms)
+   '((sql-product . ms) (sql-dialect . ms)
      (elpy-rpc-virtualenv-path . current)
-     (eltu-exuberant-ctags-additional-arguments "--python-kinds=-i" "--languages=-html,-php,-javascript"))))
+     (eltu-exuberant-ctags-additional-arguments "--python-kinds=-i"
+						"--languages=-html,-php,-javascript"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
